0ed1bdfc53f4d1ade9d7bd3093eb7218c934ae47
Cranio290 <143084676+Cranio290@users.noreply.github.com>
2023-10-12 19:20:46 +0200

    Delete src/srv/resetcanvas.php

--- src/srv/resetcanvas.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Define the file paths
-$sourceFile = 'canvasdefault.png';
-$destinationFile = 'canvas.png';
-
-// Copy the source file to the destination file
-copy($sourceFile, $destinationFile);
-?>
