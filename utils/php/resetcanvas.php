f041ccbc170365e4a62295c0818cbf7b860d9ce8
Cranio290 <143084676+Cranio290@users.noreply.github.com>
2023-10-12 21:12:56 +0200

    Add files via upload

---
+++ utils/php/resetcanvas.php
@@ -0,0 +1,8 @@
+<?php
+// Define the file paths
+$sourceFile = 'canvasdefault.png';
+$destinationFile = 'canvas.png';
+
+// Copy the source file to the destination file
+copy($sourceFile, $destinationFile);
+?>
