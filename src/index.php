a0a1614f3784335efeb2b6f5192426be4ff6d8f6
59reno59 <124379002+59reno59@users.noreply.github.com>
2023-10-15 19:34:14 +0300

    Update index.php for remove bugs

--- src/index.php
+++ src/index.php
@@ -10,12 +10,6 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-<?php 
-include 'srv/resetcanvas.php'; 
-include 'srv/setpixel.php';
-include 'srv/genpixeldata.php';
-include 'srv/socket.php';
-?>
 
 <canvas id="canvas"></canvas>
 <canvas id="canvas2" style="position: absolute; top: 0; left: 0;"></canvas>
